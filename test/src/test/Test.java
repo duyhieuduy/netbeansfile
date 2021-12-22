e0e14bffc8b19dc809e50994e1f646d8172f47e2
duyhieuduy <hieu01648476512@gmail.com>
2021-12-22 12:25:54 +0700

    a

---
+++ test/src/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+/**
+ *
+ * @author HieuPham
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("helo");
+    }
+    
+}
