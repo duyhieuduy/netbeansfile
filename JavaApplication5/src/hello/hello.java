a8334343a71a3539711e2b79e7e3d3fb92df350a
duyhieuduy <hieu01648476512@gmail.com>
2021-12-22 12:20:50 +0700

    a

---
+++ JavaApplication5/src/hello/hello.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ *
+ * @author HieuPham
+ */
+public class hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    
+}
